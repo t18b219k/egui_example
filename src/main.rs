1b9e2c63b7aab9e7e6caa47f87b010a14aa7c83f
triangle drawer <48007646+t18b219k@users.noreply.github.com>
2022-02-05 12:07:28 +0900

    upload texture

--- src/main.rs
+++ src/main.rs
@@ -85,7 +85,13 @@ async fn run(event_loop: winit::event_loop::EventLoop<()>, window: winit::window
                 egui::containers::CentralPanel::default().show(&platform.context(),|ui|{
                    debugger.ui(ui);
                 });
-
+                let tex_delta=platform.context().tex_manager().write().take_delta();
+                tex_delta.set.iter().for_each(|(k,v)|{
+                    painter.set_texture(&glow_ctx,*k,v)
+                });
+                tex_delta.free.iter().for_each(|k|{
+                    painter.free_texture(&glow_ctx,*k)
+                });
                 // End the UI frame. We could now handle the output and draw the UI with the backend.
                 let (output, paint_commands) = platform.end_frame(Some(&window));
                 let egui::Output {
